3e349d7e3e5a136c1ad6e83c9349c5f21211794c
jun <jun@kaede.local>
2009-07-28 17:14:33 +0900

    some changes

--- cedet/speedbar/speedbar-loaddefs.el
+++ cedet/speedbar/speedbar-loaddefs.el
@@ -98,7 +98,7 @@ selected.  If the speedbar frame is active, then select the attached frame.
 
 ;;;### (autoloads nil nil ("bigclock.el" "dframe.el" "sb-ant.el"
 ;;;;;;  "sb-html.el" "sb-image.el" "sb-texinfo.el" "speedbar-load.el")
-;;;;;;  (19050 35826 700030))
+;;;;;;  (19054 45383 800746))
 
 ;;;***
 
